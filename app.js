7df1cd7c00f435a4d2dc62d1702150d503440afc
Darc Mary <darcmary12@gmail.com>
2024-05-04 13:02:44 -0300

    Add mensagem validation

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function validateForm() {
     let nome = document.getElementById("nome").value.trim();
     let email = document.getElementById("email").value.trim();
     let assunto = document.getElementById("assunto").value.trim();
-    let messagem = document.getElementById("mensagem").value.trim();
+    let mensagem = document.getElementById("mensagem").value.trim();
 
     if (nome.length == 0 || nome.length > 50) {
         document.getElementById("nome-error").innerText = "Nome inválido";
@@ -19,5 +19,11 @@ function validateForm() {
         document.getElementById("assunto-error").innerText = "Assunto inválido";
     } else {
         document.getElementById("assunto-error").innerText = "";
+    }
+
+    if (mensagem.length == 0 || mensagem.length > 300) {
+        document.getElementById("mensagem-error").innerText = "Mensagem inválida";
+    } else {
+        document.getElementById("mensagem-error").innerText = "";
     }
 }   
